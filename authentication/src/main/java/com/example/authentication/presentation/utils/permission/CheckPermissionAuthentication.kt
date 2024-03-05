4e4ada94c1af1a7bc9739fc3ef0b99502abe9825
zinak <zinakola2004@gmail.com>
2024-03-06 03:58:45 +0500

    Add external permissions checker

--- authentication/src/main/java/com/example/authentication/presentation/utils/permission/CheckPermissionAuthentication.kt
+++ authentication/src/main/java/com/example/authentication/presentation/utils/permission/CheckPermissionAuthentication.kt
@@ -1,4 +1,32 @@
 package com.example.authentication.presentation.utils.permission
 
-class CheckPermissionAuthentication {
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class CheckPermissionAuthentication(private val activity: Activity) {
+
+    private val PERMISSION_REQUEST_CODE = 123
+    fun checkUserPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            callUserPermission()
+        }
+    }
+
+    private fun callUserPermission() {
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
+
 }
---
+++ core/src/main/java/com/example/core/base/dialog/SnackBarMessage.kt
@@ -0,0 +1,7 @@
+package com.example.core.base.dialog
+
+import android.app.Activity
+import android.view.View
+import androidx.core.view.KeyEventDispatcher.Component
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
